f8ad72c4d5b31d791491fe0668621075f9562283
João Vitor Lino Ferreira <joaovitorlinoferreira@gmail.com>
2026-08-24 12:19:18 -0300

    Adiciona arquivos Docker e atualiza database

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:Crislino123@localhost:5432/job_tracker"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:Crislino123@localhost:5432/job_tracker")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
