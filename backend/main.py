146dd1479e258db5a8df3ae379b4cb54473b3e09
João Vitor Lino Ferreira <joaovitorlinoferreira@gmail.com>
2026-08-24 19:19:40 -0300

    Adiciona URL da Vercel no CORS

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://job-application-tracker-virid-five.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
